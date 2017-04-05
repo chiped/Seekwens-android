0e7e7ed52fba6b6abaef0f4113c20f889c91dd10
Chinmay Pednekar <chinmay@udel.edu>
2017-04-04 20:16:37 -0700

    Use toothpick correctly

--- app/src/main/java/com/chinmay/seekwens/SeeKwensApplication.java
+++ app/src/main/java/com/chinmay/seekwens/SeeKwensApplication.java
@@ -5,9 +5,13 @@ import android.content.SharedPreferences;
 
 import java.util.UUID;
 
+import javax.inject.Inject;
+
 import toothpick.Scope;
 import toothpick.Toothpick;
 import toothpick.configuration.Configuration;
+import toothpick.registries.FactoryRegistryLocator;
+import toothpick.registries.MemberInjectorRegistryLocator;
 import toothpick.smoothie.module.SmoothieApplicationModule;
 
 public class SeeKwensApplication extends Application {
@@ -15,18 +19,21 @@ public class SeeKwensApplication extends Application {
     public static final String PREFS = "prefs";
     public static final String USER_ID_KEY = "UserID";
 
-    private Scope scope;
+    @Inject SharedPreferences preferences;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        final SharedPreferences prefs = getApplicationContext().getSharedPreferences(PREFS, MODE_PRIVATE);
-        if (prefs.getString(USER_ID_KEY, null) == null) {
-            prefs.edit().putString(USER_ID_KEY, UUID.randomUUID().toString()).apply();
-        }
-        Toothpick.setConfiguration(Configuration.forProduction());
+        Toothpick.setConfiguration(Configuration.forProduction().disableReflection());
+        FactoryRegistryLocator.setRootRegistry(new com.chinmay.seekwens.FactoryRegistry());
+        MemberInjectorRegistryLocator.setRootRegistry(new com.chinmay.seekwens.MemberInjectorRegistry());
 
-        scope = Toothpick.openScopes(this);
+        final Scope scope = Toothpick.openScopes(this);
         scope.installModules(new SmoothieApplicationModule(this));
+        Toothpick.inject(this, scope);
+
+        if (preferences.getString(USER_ID_KEY, null) == null) {
+            preferences.edit().putString(USER_ID_KEY, UUID.randomUUID().toString()).apply();
+        }
     }
 }
